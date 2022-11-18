6be74f0d25eed54200fd2aab400ad13aed82bc3c
kadirnar <kadir.nar@hotmail.com>
2022-11-18 20:14:36 +0300

    Added MANIFEST.in file.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import re
 import setuptools
 
 
-def get_requirements():
-    with open("requirements.txt", encoding="utf8") as f:
-        return f.read().splitlines()
-
-
 def get_long_description():
     base_dir = os.path.abspath(os.path.dirname(__file__))
     with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
         return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
 
 
 def get_version():
@@ -21,7 +21,6 @@ def get_version():
     version_file = os.path.join(current_dir, "bytetracker", "__init__.py")
     with io.open(version_file, encoding="utf-8") as f:
         return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
-
 
 setuptools.setup(
     name="bytetracker",
@@ -35,10 +34,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=get_requirements(),
-    include_package_data=True,
-    extras_require={
-        "dev": ["black==21.7b0", "flake8==3.9.2", "isort==5.9.2"],
-    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Operating System :: OS Independent",
